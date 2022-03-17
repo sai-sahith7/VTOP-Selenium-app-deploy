0a659cfda904576a7a495672dc714fd9141439d2
Sai Sahith <91070868+sai-sahith7@users.noreply.github.com>
2022-03-18 02:06:19 +0530

    added enable extensions

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ def get_complete_data(reg_no,vtop_password,sem_code):
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--enable-extensions")
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument('--load-extension=./extension_4_9_1_0.crx')
 #         chrome_options.add_extension("./extension_4_9_1_0.crx")
